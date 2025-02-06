dc8df83976bee73d2b6d4119d7eadc3ba8378b31
Mannat Garg <mannatgarg2006@gmail.com>
2025-02-06 14:40:56 +0530

    environment variables

--- index.js
+++ index.js
@@ -2,9 +2,30 @@ const express = require('express');
 const { resolve } = require('path');
 
 const app = express();
-const port = 3010;
+const port = 3000;
 
 app.use(express.static('static'));
+
+require('dotenv').config()
+
+const isAdmin = process.env.IS_ADMIN === 'true';
+
+if (isAdmin) {
+  console.log("Admin privileges granted.");
+} else {
+  console.log("Access restricted. Admin only.");
+}
+
+require('dotenv').config();
+
+
+app.get('/', (req, res) => {
+  if (process.env.IS_ADMIN === 'true') {
+    res.send({ message: "Welcome, Admin!", data: ["Admin Data 1", "Admin Data 2"] });
+  } else {
+    res.send({ message: "Welcome, User!", data: ["User Data 1", "User Data 2"] });
+  }
+});
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
